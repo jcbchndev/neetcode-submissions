aa456d76e3c88b2f0ef9105f08babd11f0b96584
Jacob C <44072996+jcbchndev@users.noreply.github.com>
2026-07-17 12:32:23 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // create two character count arrays, check if they're equal to each other
+
+        if (s.length() != t.length()){
+            return false;
+        }
+
+        int[] sCount = new int[26];
+        int[] tCount = new int[26];
+
+        for (int i = 0; i < s.length(); i++){
+            sCount[s.charAt(i) - 'a']++;
+            tCount[t.charAt(i) - 'a']++;
+        }
+
+        return Arrays.equals(sCount, tCount);
+    }
+}
